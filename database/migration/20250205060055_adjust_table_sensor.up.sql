069a12929a350c33212e2f812fc877892b422728
tudemaha <gede_mahardika88@yahoo.co.id>
2025-02-05 14:37:58 +0800

    fix: change typo from sensor to sensors

--- database/migration/20250205060055_adjust_table_sensor.up.sql
+++ database/migration/20250205060055_adjust_table_sensor.up.sql
@@ -1,7 +1,7 @@
 ALTER TABLE sensors MODIFY COLUMN id VARCHAR(36) NOT NULL;
 ALTER TABLE sensors MODIFY COLUMN image LONGBLOB;
-ALTER TABLE sensor DROP latitude;
-ALTER TABLE sensor DROP langitude;
+ALTER TABLE sensors DROP latitude;
+ALTER TABLE sensors DROP langitude;
 ALTER TABLE sensors ADD gateway_id VARCHAR(36) NOT NULL;
 ALTER TABLE sensors ADD node_id VARCHAR(36) NOT NULL;
 ALTER TABLE sensors ADD coordinate VARCHAR(25) NOT NULL;
